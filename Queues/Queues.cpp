80bae32b9b76266781121db7d01e2b151b94653e
an-one <anurag13june2001@gmail.com>
2021-08-28 01:28:20 +0530

    K

--- Queues/Queues.cpp
+++ Queues/Queues.cpp
@@ -1,1 +1,17 @@
-#include"Queues.h"
+#include "Queues.h"
+void Queues::enqueue(int data)
+{
+    if (head == NULL)
+    {
+        head = new Node();
+        head->data = data;
+        head->next = NULL;
+        tail = head;
+    }
+    else{
+        Node* newNode = new Node();
+        newNode->next = head;
+        head = newNode;
+    }
+}
+int Queues::dequeue() {}
--- Queues/Queues.h
+++ Queues/Queues.h
@@ -1,1 +1,16 @@
 #pragma once
+class Queues{
+    private:
+    class Node{
+        public:
+        int data;
+        Node* next;
+    };
+    public:
+    Node* head = NULL;
+    Node* tail = NULL;
+    void enqueue(int data);
+    int dequeue();
+
+
+};
