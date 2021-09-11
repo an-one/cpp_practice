2de7a92d85a2e828afef6357ed6dda3d46156ee3
an-one <annurag13june2001@gmail.com>
2021-09-11 16:44:10 +0530

    rk

---
+++ Algorithm/RabinKarp_Algorithm.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void search(char pat[], char txt[])
+{   
+    int t = strlen(txt);
+    int p = strlen(txt);
+
+}
+
+/* Driver code */
+int main()
+{
+	char txt[] = "GEEKS FOR GEEKS";
+	char pat[] = "GEEK";
+	
+	// Function Call
+	search(pat, txt);
+	return 0;
+}
+
+
