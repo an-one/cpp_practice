05553aa6a95b8da48ab8e232df6df7841a8a31a2
an-one <annurag13june2001@gmail.com>
2021-09-13 20:57:26 +0530

    f

--- Binary Search/BS.h
+++ Binary Search/BS.h
@@ -1,17 +1,35 @@
 #pragma once
-int binary_search();
-int searchB(int arr[],int size,int target){
-    int low = 0;
+
+int binary_search(int numbers[],int size,int target){
+   int low = 0;
   int high = size - 1;
   int mid = 0;
   while (low <= high) {
     mid = (high + low) / 2;
 
-    if (target > arr[mid]) {
+    if (target > numbers[mid]) {
       low = mid + 1;
-    } else if (target < arr[mid]) {
+    } else if (target < numbers[mid]) {
       high = mid - 1;
     } else {
       return mid;
     }
+  }
+
+  return -1;
+}
+int binary_search_recur(int target, int numbers[], int low, int high) {
+  if (low > high) {
+    return -1;
+  }
+
+  int mid = (high + low) / 2;
+
+  if (target > numbers[mid]) {
+    return binary_search_recur(target, numbers, mid + 1, high);
+  } else if (target < numbers[mid]) {
+    return binary_search_recur(target, numbers, low, mid - 1);
+  } else {
+    return mid;
+  }
 }
--- Binary Search/BS_main.cpp
+++ Binary Search/BS_main.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 #include"BS.h"
-using namespace std;
+
+
 int main(){
     int sizee = 14;
     int arr[sizee] = {1, 2 ,3, 4, 5,6 ,23,34,45,56,67,78,89,90};
-    cout<<searchB(arr,sizee,89);
+    std::cout<<binary_search_recur(99,arr,0,13);
 }
