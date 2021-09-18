100c0fb59379ac4ae8854ffbcae8fe185d15591c
an-one <annurag13june2001@gmail.com>
2021-09-18 08:19:55 +0530

    hello

--- Binary Search Tree/BST.cpp
+++ Binary Search Tree/BST.cpp
@@ -1,1 +1,43 @@
 #include "BST.h"
+void Bst::insertBST(int data){
+    if(!isEmpty()){
+        Node* temp = root;
+        while(1){
+           if(data>temp->data){
+               if(temp->right != NULL){
+                   temp = temp->right;
+               }else{
+                   temp->right = new Node();
+                   temp->right->data = data;
+                   temp->right->right = NULL;
+                   temp->right->left = NULL;
+                   break;
+               }
+           }
+           else{
+               if(temp->left != NULL){
+                   temp = temp->left;
+               }else{
+                   temp->left = new Node();
+                   temp->left->data = data;
+                   temp->left->right = NULL;
+                   temp->left->left = NULL;
+                   break;
+               }
+
+           }
+        }
+    }else{
+        root->data = data;
+        root->left = NULL;
+        root->right = NULL;
+    }
+
+}
+bool Bst::isEmpty(){
+    if(root == NULL){
+        return true;
+    }else{
+        return false;
+    }
+}
--- Binary Search Tree/BST.h
+++ Binary Search Tree/BST.h
@@ -1,1 +1,14 @@
 #pragma once
+class Bst{
+    class Node{
+        public:
+        int data;
+        Node* left;
+        Node* right;
+    };
+    Node* root = NULL;
+    public:
+    bool isEmpty();
+    void insertBST(int);
+
+};
