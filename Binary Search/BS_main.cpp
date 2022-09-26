94f485ccc1c7c30b001c70dd1caf76e1f3d38bd0
Anurag Singh <66548166+an-one@users.noreply.github.com>
2022-09-26 13:13:17 +0530

    no message

--- Binary Search/BS_main.cpp
+++ Binary Search/BS_main.cpp
@@ -4,6 +4,6 @@
 
 int main(){
     int sizee = 14;
-    int arr[sizee] = {1, 2 ,3, 4, 5,6 ,23,34,45,56,67,78,89,90};
+    int arr[14] = {1, 2 ,3, 4, 5,6 ,23,34,45,56,67,78,89,90};
     std::cout<<binary_search_recur(99,arr,0,13);
 }
